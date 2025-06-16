db32aecb705cf6ceba1c8d9adacc901bee590b4d
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-06-16 08:00:36 +0545

    update: demo website

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
-
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-],
+  plugins: [react(), tailwindcss()],
   build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'EsewaReact',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'cjs', 'umd']
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    },
+    outDir: 'dist',
     sourcemap: true
   },
+  root: 'src',
+  publicDir: 'public'
 })
